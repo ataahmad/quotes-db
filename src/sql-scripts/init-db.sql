929576e0976ede7b72e7ca54c426e72326417c41
Ata <ataahmad@Atas-MBP.lan>
2024-01-08 21:05:51 -0800

    changes the authentication method of the root user for mysql npm package compatibility

--- src/sql-scripts/init-db.sql
+++ src/sql-scripts/init-db.sql
@@ -3,3 +3,7 @@ CREATE TABLE quotes (
     quote TEXT NOT NULL,
     author VARCHAR(255)
 );
+
+-- Allows for default connection method from mysql npm package
+ALTER USER 'root' IDENTIFIED WITH 'mysql_native_password' BY 'rootpassword';
+FLUSH PRIVILEGES;
