dc6fff7c2cfa6095ad861033ce34d93ed10a310b
Ata <ataahmad@Atas-MBP.lan>
2024-01-12 08:26:13 -0800

    Updates script to take in user input

--- src/scripts/seedFromAPI.ts
+++ src/scripts/seedFromAPI.ts
@@ -1,6 +1,8 @@
 import { SeedAPIQuote } from '../interfaces/seedAPI/seedAPIResponse'
 import { DBQuote } from '../interfaces/database/quote';
 import { mapApiQuoteToQuote, populateDB } from './scriptHelpers'
+import * as readline from 'readline';
+
 
 
 const seedQuotes = async (limit: number = 1) => {
@@ -23,4 +25,32 @@ const seedQuotes = async (limit: number = 1) => {
   }
 }
 
-seedQuotes(5)
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let limitNumber: number
+
+rl.question('How many quotes do you want to populate?\n', (input) => {
+
+  let numInput: number
+  if (typeof input === "string" && !Number.isNaN(Number(input))) {
+    numInput = Number(input);
+  } else {
+    console.error("Invalid Number Entered")
+    rl.close()
+    return
+  }
+
+
+  if (numInput > 1000) {
+    console.log("Limit is set at 1000 quotes\nSeeding 1000 quotes...")
+    seedQuotes(1000)
+  } else {
+    console.log(`Seeding ${numInput} quotes...`)
+    seedQuotes(numInput)
+  }
+  
+  rl.close();
+});
